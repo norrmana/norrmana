ec3ca8545b5398ece6050b817db1276b65f85831
André Norrman <andre.norrman@cgi.com>
2015-05-26 15:51:43 +0200

    Simple test class

---
+++ src/org/norrmana/gitter/HelloGit.java
@@ -0,0 +1,15 @@
+package org.norrmana.gitter;
+
+public class HelloGit {
+
+    @Override
+    public String toString() {
+        return "Hello World!!!  ";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HelloGit().toString());
+
+    }
+
+}
