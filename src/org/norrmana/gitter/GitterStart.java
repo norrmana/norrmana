ed680a09c8bf98290fbc25e80f6cbf33e140e513
André Norrman <andre.norrman@cgi.com>
2015-05-26 13:02:45 +0200

    Small update

---
+++ src/org/norrmana/gitter/GitterStart.java
@@ -0,0 +1,15 @@
+package org.norrmana.gitter;
+
+public class GitterStart {
+
+    @Override
+    public String toString() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new GitterStart().toString());
+
+    }
+
+}
