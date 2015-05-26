165dea99b9a01bc05af47c300606d80bcc11b213
André Norrman <andre.norrman@cgi.com>
2015-05-26 14:46:41 +0200

    Update 3(whats up)

--- src/org/norrmana/gitter/GitterStart.java
+++ src/org/norrmana/gitter/GitterStart.java
@@ -4,7 +4,7 @@ public class GitterStart {
 
     @Override
     public String toString() {
-        return "Hello World!!!";
+        return "Hello World!!! Whats up?";
     }
 
     public static void main(String[] args) {
